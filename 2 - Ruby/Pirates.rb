9d5b5f0fb3f94c39eca44b4c9891e88c2c7b5f02
Joice Cazanoski Gomes <cgjoice@gmail.com>
2018-05-18 11:46:39 -0300

    Correções
    -Mostra para o jogador o mapa apenas com a localização do próprio barco e locais onde já foram atingidos 
    -4 para inimigo e 5 para tiro na água
    -Posição para atirar começa em 1 e termina em 8

--- 2 - Ruby/Pirates.rb
+++ 2 - Ruby/Pirates.rb
@@ -2,8 +2,10 @@
   Map identifiers
   0 - Water
   1 - Our ship
-  2 - EITC enemy ship
+  2 - EIC enemy ship
   3 - Flying Dutch
+  4 - Hit 
+  5 - Shoot at water
 =end
 
 require 'matrix'
@@ -19,7 +21,7 @@ class SeaMap
     @matrix = Array.new(8) { Array.new(3, 0) } 
     insertShips()
   end
-  
+
   def getCoordinates
     coord = {}
     coord['x'] = rand(7)
@@ -28,32 +30,36 @@ class SeaMap
   end
   
   def shoot(x, y)
-    if (x > 7 or y > 2 or x < 0 or y < 0)
+    if (x > 8 or y > 3 or x <= 0 or y <= 0)
       puts "Posicão inválida!"
-    elsif (matrix[x][y] == 1)
+    elsif (matrix[x-1][y-1] == 1)
       puts "Voce está aqui."
-    elsif (matrix[x][y] == 2 or matrix[x][y] == 3)
+    elsif (matrix[x-1][y-1] == 2 or matrix[x-1][y-1] == 3)
       puts "Acertou o inimigo!"
-      self.matrix[x][y] = 0
+      self.matrix[x-1][y-1] = 4
     else
       puts "Tiro na água!"
+      self.matrix[x-1][y-1] = 5
     end
   end
   
   def won
-    enemyCount = 0
+    blackPearl = 0
+    ghost = 0
     for y in 0..2
       for x in 0..7
-        if (self.matrix[x][y].between?(2, 3))
-          enemyCount += 1
+        if (self.matrix[x][y] == 2)
+          blackPearl += 1
+        elsif (self.matrix[x][y] == 3)
+          ghost += 1
         end
       end
     end
-    if (enemyCount != 0)
-      return false
+    if (blackPearl == 0 or ghost == 0)
+      return true
     end
 
-    return true
+    return false
   end
   
   def verifyCoordinates(coord, size, direction)
@@ -110,32 +116,41 @@ class SeaMap
       puts output
     end
   end
+
+   def showToPlayer
+    for y in 0..2
+      output = ""
+      for x in 0..7
+        if matrix[x][y] == 1 or matrix[x][y] == 4 or matrix[x][y] == 5
+          output = output + "\t" + matrix[x][y].to_s
+        else
+          output = output + "\t" + '0'
+        end
+      end
+      puts output 
+    end
+  end
 end
 
 map = SeaMap.new()
-kraken = rand(7..24)
+kraquen = rand(7..24)
 
 puts "O jogo pirata vai começar!"
-puts "LEGENDA: "
-puts "  0 - Água "
-puts "  1 - Você"
-puts "  2 - Navio da Cia. das Índias Orientais"
-puts "  3 - Holandês Voador"
-puts ""
-
-map.print()
+map.showToPlayer
+#map.print()
 
 won = false
 count = 0
 
-while (won == false and count < kraken) do
+while (won == false and count < kraquen) do
   puts "Informe a coluna (eixo X) na qual você deseja atirar:"
-  x = gets.chomp.to_i - 1
+  x = gets.chomp.to_i
   puts "Informe a linha (eixo Y) na qual você deseja atirar:"
-  y = gets.chomp.to_i - 1
+  y = gets.chomp.to_i
 
   map.shoot(x, y);
-  map.print()
+  map.showToPlayer()
+  #map.print()
 
   if (map.won())
     won = true
@@ -147,9 +162,5 @@ end
 if (won == true)
   puts "Você é um baita pirata! Ganhou sapequinha!"
 elsif
-  if (count == kraken)
-    puts "O Kraken foi solto!"
-  end
-
   puts "No céu tem pão? E morreu!"
 end
